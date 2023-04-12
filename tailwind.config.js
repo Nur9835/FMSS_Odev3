a01acb38d230cd62e19291bde668ce00ec38d373
Nur <90011276+Nur9835@users.noreply.github.com>
2023-04-12 06:14:18 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+backgroundImage:{
+  'bulut':"url('/src/img/bulut.png')",
+},
+
+
+    },
+  },
+  plugins: [],
+}
+
